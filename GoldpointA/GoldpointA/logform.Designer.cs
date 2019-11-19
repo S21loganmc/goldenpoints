3760457c6d9c3a5518474764450ec20f2298d22f
S21loganmc <1940652847@qq.com>
2019-11-19 21:13:43 +0800

    ALPHA II

---
+++ GoldpointA/GoldpointA/logform.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Goldpoint1._0
+{
+    partial class logform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usrname_textbox = new System.Windows.Forms.TextBox();
+            this.usrpwd_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usrname_textbox
+            // 
+            this.usrname_textbox.Location = new System.Drawing.Point(152, 34);
+            this.usrname_textbox.Name = "usrname_textbox";
+            this.usrname_textbox.Size = new System.Drawing.Size(219, 25);
+            this.usrname_textbox.TabIndex = 0;
+            // 
+            // usrpwd_textbox
+            // 
+            this.usrpwd_textbox.Location = new System.Drawing.Point(152, 80);
+            this.usrpwd_textbox.Name = "usrpwd_textbox";
+            this.usrpwd_textbox.PasswordChar = '*';
+            this.usrpwd_textbox.Size = new System.Drawing.Size(219, 25);
+            this.usrpwd_textbox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "用户名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "密码";
+            // 
+            // loginbutton
+            // 
+            this.loginbutton.Location = new System.Drawing.Point(203, 129);
+            this.loginbutton.Name = "loginbutton";
+            this.loginbutton.Size = new System.Drawing.Size(75, 23);
+            this.loginbutton.TabIndex = 4;
+            this.loginbutton.Text = "登陆";
+            this.loginbutton.UseVisualStyleBackColor = true;
+            this.loginbutton.Click += new System.EventHandler(this.loginbutton_Click);
+            // 
+            // logform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 175);
+            this.Controls.Add(this.loginbutton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usrpwd_textbox);
+            this.Controls.Add(this.usrname_textbox);
+            this.Name = "logform";
+            this.Text = "登陆";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usrname_textbox;
+        private System.Windows.Forms.TextBox usrpwd_textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button loginbutton;
+    }
+}
